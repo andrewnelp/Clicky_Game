6ce0ce591ed231de11c12420890c455e4809cd97
Andrey Nelepov <nelepow@gmail.com>
2019-07-24 17:10:06 -0400

    Updates

---
+++ precache-manifest.e87e3dc571f123b4598c0869277ac205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865bfb7fdd13064bf66f151b47f74902",
+    "url": "/Clicky_Game/index.html"
+  },
+  {
+    "revision": "8da6ade56a7b9aad9780",
+    "url": "/Clicky_Game/static/css/main.55250781.chunk.css"
+  },
+  {
+    "revision": "075de474c72d3b0b368e",
+    "url": "/Clicky_Game/static/js/2.80799cc3.chunk.js"
+  },
+  {
+    "revision": "8da6ade56a7b9aad9780",
+    "url": "/Clicky_Game/static/js/main.cb61b694.chunk.js"
+  },
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  }
+]);
